a278b54d3deccc5664572cce8bc806dc6edf676e
Murtle <myrthe.e@hotmail.nl>
2021-06-21 21:42:28 +0200

    Changed styling

--- src/pages/SchedulePage/index.js
+++ src/pages/SchedulePage/index.js
@@ -6,10 +6,12 @@ function SchedulePage(props) {
   const [selectedDate, setSelectedDate] = useState();
   const [validationMessage, setValidationMessage] = useState(false);
   const [meetings, setMeetings] = useState();
+  const [activeDate, setActiveDate] = useState();
 
   function onClickHandler(e) {
     setSelectedDate(e)
     setValidationMessage(false)
+    setActiveDate(selectedDate)
   }
 
   const onSubmit = () => {
@@ -24,58 +26,67 @@ function SchedulePage(props) {
   return (
     <div className="Schedule">
       <div className="container">
-        <h1>Meetings</h1>
-          <div className="overview">
-            {meetings 
-              ? 
+        <div className="row">
+          <div className="col-md-6 mx-auto">
+            <h1>Plan een gesprek in</h1>
+            <h2>Tijd om zaken <span>te regelen</span> voor een goede re-intregratie.</h2>
+          </div>
+        </div>
+
+        <div className="date-picker row">
+          <button className="date-button col-md-2" onClick={() => onClickHandler('11:00, 12 juli 2021')}>
+            <div className={`date-card ${activeDate === '11:00, 12 juli 2021' ? 'active' : ''}`}>
+              <p><span>11:00 uur</span></p>
+              <p>12 juli 2021</p>
+            </div>
+          </button>
+          <button className="date-button col-md-2" onClick={() => onClickHandler('14:30, 12 juli 2021')}>
+            <div className={`date-card ${activeDate === '14:30, 12 juli 2021' ? 'active' : ''}`}>
+              <p><span>14:30 uur</span></p>
+              <p>12 juli 2021</p>
+            </div>
+          </button>
+          <button className="date-button col-md-2" onClick={() => onClickHandler('10:00, 13 juli 2021')}>
+            <div className={`date-card ${activeDate === '10:00, 13 juli 202' ? 'active' : ''}`}>
+              <p><span>10:00 uur</span></p>
+              <p>13 juli 2021</p>
+            </div>
+          </button>
+          <button className="date-button col-md-2" onClick={() => onClickHandler('09:30, 14 juli 2021')}>
+            <div className={`date-card ${activeDate === '09:30, 14 juli 2021' ? 'active' : ''}`}>
+              <p><span>09:30 uur</span></p>
+              <p>14 juli 2021</p>
+            </div>
+          </button>
+          <button className="date-button col-md-2" onClick={() => onClickHandler('11:30, 15 juli 2021')}>
+            <div className={`date-card ${activeDate === '11:30, 15 juli 2021' ? 'active' : ''}`}>
+              <p><span>11:30 uur</span></p>
+              <p>15 juli 2021</p>
+            </div>
+          </button>
+        </div>
+        
+        {validationMessage ? <p className="validation-message">Vergeet niet een datum te selecteren...</p> : '' }
+
+        <div className="row">
+          <div className="col-md-12">
+            <div className="overview">
+              { meetings 
+                ? 
                 <div className="open-meetings">
                   <p>Mijn Meetings:</p>
                   <Link to='/fcg-nfig-fpy'>
                     <button>{meetings}</button>
                   </Link>
                 </div>
-              : 'U heeft nog geen meetings ingepland...'
-            }
+                : 'U heeft nog geen meetings ingepland...'
+              }
+            </div>
           </div>
-          <br/><br/>
-          <h3>Nieuwe meeting inplannen</h3>
-          <div className="date-picker">
-            <button className="date-button" onClick={() => onClickHandler('11:00, 12 juli 2021')}>
-              <div className="date-card">
-                <p>11:00 uur</p>
-                <p>12 juli 2021</p>
-              </div>
-            </button>
-            <button className="date-button" onClick={() => onClickHandler('14:30, 12 juli 2021')}>
-              <div className="date-card">
-                <p>14:30 uur</p>
-                <p>12 juli 2021</p>
-              </div>
-            </button>
-            <button className="date-button" onClick={() => onClickHandler('10:00, 13 juli 2021')}>
-              <div className="date-card">
-                <p>10:00 uur</p>
-                <p>13 juli 2021</p>
-              </div>
-            </button>
-            <button className="date-button" onClick={() => onClickHandler('09:30, 14 juli 2021')}>
-              <div className="date-card">
-                <p>09:30 uur</p>
-                <p>14 juli 2021</p>
-              </div>
-            </button>
-            <button className="date-button" onClick={() => onClickHandler('11:30, 15 juli 2021')}>
-              <div className="date-card__last">
-                <p>11:30 uur</p>
-                <p>15 juli 2021</p>
-              </div>
-            </button>
-          </div>
-          {validationMessage ? <p className="validation-message">Vergeet niet een datum te selecteren...</p> : '' }
-          
+        </div>
 
-          <button onClick={() => onSubmit()} type="button" className="btn btn-success">Plan een nieuwe meeting</button>
-        
+        <button onClick={() => onSubmit()} type="button" className="btn btn-success">Plan een nieuwe meeting</button>
+    
         {/* <button onClick={() => {props.videoCall(true)}} type="button" className="btn btn-secondary">Go to meeting</button> */}
       </div>
     </div>
